43525555799fc3c075674f4679ef78c0f93c7864
AmadeusMoon <sunnysneezer@gmail.com>
2024-01-04 19:59:30 +0100

    Create backbone

---
+++ src/state/reducers/books.ts
@@ -0,0 +1,4 @@
+// Reading progress per book
+// Last page read per book
+// Scroll type when reading
+// Type of books read for recommendation ( reducer to find out most used tags add a 0.5 per tags used in search)
